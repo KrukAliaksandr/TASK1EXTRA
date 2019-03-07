b90d713ec69efbc0f9bb44a3a2a9d3c2cb0ac644
Aliaksandr_Kruk <krukaliaksandr@gmail.com>
2019-03-07 14:31:49 +0300

    Remove comments.Add remove function .Remove testfiles

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ class BuisnessRecord extends PersonalRecord {
   }
 }
 // @TODO change function to just parse proper object from String
-const transformArgumentsIntoRecords = (consoleArgument) => {
+const transformIntoListOfRecords = (consoleArgument) => {
   const recordsArray = [];
   consoleArgument.forEach((record) => {
     const dataArray = record.split('-');
@@ -40,7 +40,7 @@ const transformArgumentsIntoRecords = (consoleArgument) => {
 
 const writeRecrod = (args, requiredFile) => {
   if ('list' in args) {
-    requiredFile.push(new Organization(args.title, transformArgumentsIntoRecords(args.list)));
+    requiredFile.push(new Organization(args.title, transformIntoListOfRecords(args.list)));
   } else {
     if ('description' in args) {
       requiredFile.push(new BuisnessRecord(args.title, args.phone, args.description));
@@ -50,13 +50,24 @@ const writeRecrod = (args, requiredFile) => {
   });
 };
 
-const ListNotes = (args, requiredFile) => {
+const listRecords = (args, requiredFile) => {
   requiredFile.forEach((node) => {
     console.log(node);
   });
 };
 
-// add type
+const removeRecords = (args, requiredFile) => {
+  const requiredRecord = new Organization(args.title, transformIntoListOfRecords(args.list));
+  console.log(requiredRecord);
+
+  const result = requiredFile.filter((fileRecord) => {
+    return Object.toJSON(requiredRecord) !== Object.toJSON(fileRecord);
+  });
+  fs.writeFileSync(args.file + '.json', JSON.stringify(result, null, '\t'), 'utf8', () => {
+  });
+};
+
+// @TODO add type
 // eslint-disable-next-line no-unused-expressions
 todo.command('Add', `makes an action with a file`, function (yargs) {
   return yargs.options({
@@ -101,6 +112,49 @@ function (argv) {
     console.log(err.message);
   }
 })
+  .command('Remove', `makes an action with a file`, function (yargs) {
+    return yargs.options({
+      'list': {
+        alias: 'l',
+        describe: 'specifies the list of the records to use',
+        conflicts: 'phone',
+        demandOption: false,
+        array: true
+      },
+      'file': {
+        alias: 'f',
+        describe: 'path to file',
+        demandOption: true
+      },
+      'title': {
+        alias: 't',
+        describe: 'node title',
+        implies: 'phone' | 'l',
+        demandOption: true
+      },
+      'phone': {
+        alias: 'p',
+        describe: 'node phone',
+        conflicts: 'list',
+        demandOption: false
+      },
+      'description': {
+        alias: 'd',
+        describe: 'node phone',
+        implies: 'phone',
+        conflicts: 'list',
+        demandOption: false
+      }
+    });
+  },
+  function (argv) {
+    const jsonObject = require('./' + argv.file + '.json');
+    try {
+      removeRecords(argv, jsonObject);
+    } catch (err) {
+      console.log(err.message);
+    }
+  })
   .command('List', 'makes an action with a file', function (yargs) {
     return yargs.options({
       'file': {
@@ -112,7 +166,7 @@ function (argv) {
   },
   function (argv) {
     const jsonObject = require('./' + argv.file + '.json');
-    ListNotes(argv, jsonObject);
+    listRecords(argv, jsonObject);
   }
   )
   .help()
